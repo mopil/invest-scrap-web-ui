1e19934ffcaec8bb16d89f49ec20c26bcc462a90
Seong Heung Bae <mopil1102@gmail.com>
2026-03-13 01:37:38 +0900

    Format timestamps in KST

--- src/utils/format.js
+++ src/utils/format.js
@@ -17,7 +17,16 @@ export function formatDateTime(value) {
     return String(value);
   }
 
-  return date.toLocaleString("ko-KR");
+  return new Intl.DateTimeFormat("ko-KR", {
+    timeZone: "Asia/Seoul",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+  }).format(date);
 }
 
 export function buildDocumentUrl(boardId, documentId) {
